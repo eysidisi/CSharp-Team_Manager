342c925a15f98afeef2eae69b9954194226c15b1
ali elmas <a.ihsan.elmas@gmail.com>
2022-03-15 00:03:10 +0300

    Start implementing integration tests

---
+++ TeamManager.Service.IntegrationTest/DB/SQLite/NewUserPageServiceTests.cs
@@ -0,0 +1,77 @@
+using Xunit;
+using TeamManager.Service.Test;
+using TeamManager.Service.Test.HelperMethods.SQLiteDB;
+using TeamManager.Service.Management.Database;
+using TeamManager.Service.Management;
+using TeamManager.Service.Models;
+using System.Data.SQLite;
+using Dapper.Contrib.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TeamManager.Service.IntegrationTest.DB.SQLite
+{
+    public class NewUserPageServiceTests
+    {
+        [Fact]
+        public void SaveNewUser_EmptyDB_SavesNewUser()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            NewUserPageService newUserPageService = new NewUserPageService(connection);
+
+            User user = new User() { Name = "userName", Surname = "userSurname" };
+
+            // Act
+            newUserPageService.SaveNewUser(user);
+
+            // Assert
+            List<User> users;
+
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                users = cnn.GetAll<User>().ToList();
+            }
+
+            Assert.Contains(users, u => u.Name == user.Name && u.Surname == user.Surname);
+
+            helperMethods.DeleteDB(dbPath);
+        }
+
+        [Fact]
+        public void SaveNewUser_NotEmptyDB_SavesNewUser()
+        {
+            throw new NotImplementedException();
+
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            NewUserPageService newUserPageService = new NewUserPageService(connection);
+
+            User user = new User() { Name = "userName", Surname = "userSurname" };
+
+            // Act
+            newUserPageService.SaveNewUser(user);
+
+            // Assert
+            List<User> users;
+
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                users = cnn.GetAll<User>().ToList();
+            }
+
+            Assert.Contains(users, u => u.Name == user.Name && u.Surname == user.Surname);
+
+            helperMethods.DeleteDB(dbPath);
+        }
+    }
+}
---
+++ TeamManager.Service.IntegrationTest/DB/SQLite/UserDetailsPageServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using Dapper.Contrib.Extensions;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using TeamManager.Service.Management;
+using TeamManager.Service.Management.Database;
+using TeamManager.Service.Management.UserServices;
+using TeamManager.Service.Models;
+using TeamManager.Service.Test.HelperMethods.SQLiteDB;
+using Xunit;
+
+namespace TeamManager.Service.IntegrationTest.DB.SQLite
+{
+    public class UserDetailsPageServiceTests
+    {
+        [Fact]
+        public void GetTeamsThatUserIn_EmptyDB_ReturnsEmptyList()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            UserDetailsPageService userDetailsPageService = new UserDetailsPageService(connection);
+
+            User user = new User();
+
+            // Act
+            var teams = userDetailsPageService.GetTeamsThatUserIn(user);
+
+            // Assert
+            Assert.Empty(teams);
+        }
+
+        [Fact]
+        public void GetTeamsThatUserIn_UserIsInATeam_ReturnsEmptyList()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            UserDetailsPageService userDetailsPageService = new UserDetailsPageService(connection);
+
+            Team team = new Team() { ID = 1, Name = "team" };
+            User user = new User() { ID = 1, Name = "userName" };
+            UserIDToTeamID userIDToTeamID = new UserIDToTeamID() { ID = 1, UserID = 1, TeamID = 1 };
+
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                cnn.Insert(team);
+                cnn.Insert(user);
+                cnn.Insert(userIDToTeamID);
+            }
+
+            // Act
+            var teams = userDetailsPageService.GetTeamsThatUserIn(user);
+
+            // Assert
+            Assert.Single(teams);
+            Assert.Contains(teams, t => t.ID == team.ID && t.Name == team.Name);
+        }
+    }
+}
