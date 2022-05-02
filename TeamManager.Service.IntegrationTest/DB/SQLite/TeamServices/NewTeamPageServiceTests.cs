ef3bdff3afa8854d12d4c8169eedcef68404f93a
ali elmas <a.ihsan.elmas@gmail.com>
2022-05-02 23:03:24 +0300

    Add new integration test cases

---
+++ TeamManager.Service.IntegrationTest/DB/SQLite/TeamServices/NewTeamPageServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamManager.Service.Management;
+using TeamManager.Service.Management.Database;
+using TeamManager.Service.Management.TeamServices;
+using TeamManager.Service.Models;
+using TeamManager.Service.Test.HelperMethods.SQLiteDB;
+using Xunit;
+
+namespace TeamManager.Service.IntegrationTest.DB.SQLite.TeamServices
+{
+    public class NewTeamPageServiceTests
+    {
+        [Fact]
+        public void SaveTeam_NoTeamExistsInDB_AddsTeam()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateEmptyTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+
+            NewTeamPageService newTeamPageService = new NewTeamPageService(connection);
+
+            Team team = new Team() { Name = "Team1" };
+
+            // Act
+            newTeamPageService.SaveTeam(team);
+
+            // Assert
+            List<Team> teams;
+
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                teams = cnn.GetAll<Team>().ToList();
+            }
+
+            Assert.Contains(teams, t => t.Name == team.Name);
+
+            helperMethods.DeleteDB(dbPath);
+        }
+    }
+}
---
+++ TeamManager.Service.IntegrationTest/DB/SQLite/TeamServices/TeamPageServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamManager.Service.Management;
+using TeamManager.Service.Management.Database;
+using TeamManager.Service.Models;
+using TeamManager.Service.Test.HelperMethods.SQLiteDB;
+using Xunit;
+
+namespace TeamManager.Service.IntegrationTest.DB.SQLite.TeamServices
+{
+    public class TeamPageServiceTests
+    {
+        [Fact]
+        public void GetAllTeams_DBHasNoTeams_ReturnsEmptyList()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateEmptyTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            TeamPageService teamPageService = new TeamPageService(connection);
+
+            // Act
+            var teams = teamPageService.GetAllTeams();
+
+            // Assert
+            Assert.Empty(teams);
+
+            helperMethods.DeleteDB(dbPath);
+        }
+
+        [Fact]
+        public void GetAllTeams_DBHasTeams_ReturnsTeams()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateEmptyTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            List<Team> expectedTeams = new List<Team>()
+            {
+                new Team(){Name="team1"},
+                new Team(){Name="team2"}
+            };
+
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                cnn.Insert(expectedTeams);
+            }
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            TeamPageService teamPageService = new TeamPageService(connection);
+
+            // Act
+            var actualTeams = teamPageService.GetAllTeams();
+
+            // Assert
+            foreach (var expectedTeam in expectedTeams)
+            {
+                Assert.Contains(actualTeams, a => a.Name == expectedTeam.Name);
+            }
+
+            helperMethods.DeleteDB(dbPath);
+        }
+
+        [Fact]
+        public void DeleteTeam_DBHasTheTeam_DeletesTeam()
+        {
+            // Arrange
+            HelperMethods helperMethods = new HelperMethods();
+            var dbPath = helperMethods.CreateEmptyTestDB_ReturnFilePath();
+            string connString = $"Data Source={dbPath}";
+
+            Team teamToDelete = new Team() { Name = "teamToDelete", ID = 0 };
+
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                cnn.Insert(teamToDelete);
+            }
+
+            ManagerSQLiteConnetion connection = new ManagerSQLiteConnetion(connString);
+            TeamPageService teamPageService = new TeamPageService(connection);
+
+            // Act
+            teamPageService.DeleteTeam(teamToDelete);
+
+            // Assert
+            List<Team> teamsLeftInDB;
+            using (var cnn = new SQLiteConnection(connString))
+            {
+                teamsLeftInDB = cnn.GetAll<Team>().ToList();
+            }
+
+            Assert.DoesNotContain(teamsLeftInDB, t => t.Name == teamToDelete.Name);
+
+            helperMethods.DeleteDB(dbPath);
+        }
+    }
+}
