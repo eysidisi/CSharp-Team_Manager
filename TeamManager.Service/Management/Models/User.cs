8c8bb6373051dbec80ff2e6bfe78bf641f182ee9
ali elmas <a.ihsan.elmas@gmail.com>
2022-05-05 22:05:37 +0300

    Implement IEquatable in User class

--- TeamManager.Service/Management/Models/User.cs
+++ TeamManager.Service/Management/Models/User.cs
@@ -1,9 +1,10 @@
 ﻿using Dapper.Contrib.Extensions;
+using System;
 
 namespace TeamManager.Service.Models
 {
     [Table("Users")]
-    public class User
+    public class User : IEquatable<User>
     {
         public int ID { get; set; }
         public string Name { get; set; }
@@ -14,6 +15,21 @@ namespace TeamManager.Service.Models
 
         public User()
         {
+        }
+
+        public bool Equals(User? otherUser)
+        {
+            if (otherUser == null)
+            {
+                return false;
+            }
+
+            return otherUser.ID == ID &&
+                   otherUser.Name == Name &&
+                   otherUser.Surname == Surname &&
+                   otherUser.CreationDate == CreationDate &&
+                   otherUser.PhoneNumber == PhoneNumber &&
+                   otherUser.Title == Title;
         }
     }
 }
