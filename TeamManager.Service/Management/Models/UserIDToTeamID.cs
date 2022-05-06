96800eef5161a39df2daa789fea9ab7d6f4356b9
ali elmas <a.ihsan.elmas@gmail.com>
2022-05-06 11:34:38 +0300

    Implement IEquatable for UserIDToTeamID

--- TeamManager.Service/Management/Models/UserIDToTeamID.cs
+++ TeamManager.Service/Management/Models/UserIDToTeamID.cs
@@ -8,14 +8,26 @@ using System.Threading.Tasks;
 namespace TeamManager.Service.Models
 {
     [Table("UserID_To_TeamID")]
-    public class UserIDToTeamID
+    public class UserIDToTeamID : IEquatable<UserIDToTeamID>
     {
         public int ID { get; set; }
-        public int  UserID{ get; set; }
-        public int  TeamID{ get; set; }
+        public int UserID { get; set; }
+        public int TeamID { get; set; }
         public UserIDToTeamID()
         {
 
+        }
+
+        public bool Equals(UserIDToTeamID? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return ID == other.ID && 
+                   UserID == other.UserID && 
+                   TeamID == other.TeamID;
         }
     }
 }
