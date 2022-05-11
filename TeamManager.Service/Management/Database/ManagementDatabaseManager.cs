3fac8ea5f281c4dc792c0206944786be55711423
ali elmas <a.ihsan.elmas@gmail.com>
2022-05-11 20:30:00 +0300

    Refactor management database connection

---
+++ TeamManager.Service/Management/Database/ManagementDatabaseManager.cs
@@ -0,0 +1,130 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using System.Data;
+using System.Data.SQLite;
+using TeamManager.Service.Models;
+
+namespace TeamManager.Service.Management.Database
+{
+    public abstract class ManagementDatabaseManager : IManagementDatabaseConnection
+    {
+        List<User> users = null;
+        List<Team> teams = null;
+        List<UserIDToTeamID> userIDsToTeamIDs = null;
+        protected IDbConnection dbConnection;
+        protected string connString;
+
+        public ManagementDatabaseManager(string connString)
+        {
+            this.connString = connString;
+            CreateConnection();
+        }
+
+        public void SaveUser(User user)
+        {
+            dbConnection.Insert(user);
+
+            if (users != null)
+            {
+                users.Add(user);
+            }
+        }
+
+        public bool DeleteUser(User user)
+        {
+            if (dbConnection.Delete(user) == false)
+            {
+                return false;
+            }
+
+            else
+            {
+                if (users != null)
+                {
+                    users.Remove(user);
+                }
+                return true;
+            }
+        }
+
+        public List<User> GetAllUsers()
+        {
+            if (users == null)
+            {
+                users = dbConnection.GetAll<User>().ToList();
+            }
+
+            return users;
+        }
+
+        public void SaveTeam(Team team)
+        {
+            dbConnection.Insert(team);
+            if (teams != null)
+            {
+                teams.Add(team);
+            }
+        }
+
+        public List<Team> GetAllTeams()
+        {
+            if (teams == null)
+            {
+                teams = dbConnection.GetAll<Team>().ToList();
+            }
+
+            return teams;
+        }
+
+        public bool DeleteTeam(Team team)
+        {
+            if (dbConnection.Delete(team) == false)
+            {
+                return false;
+            }
+            else
+            {
+                if (teams != null)
+                {
+                    teams.Remove(team);
+                }
+                return true;
+            }
+        }
+
+        public List<UserIDToTeamID> GetAllUserIDToTeamID()
+        {
+            if (userIDsToTeamIDs == null)
+            {
+                userIDsToTeamIDs = dbConnection.GetAll<UserIDToTeamID>().ToList();
+            }
+            return userIDsToTeamIDs;
+        }
+
+        public void SaveUserIDToTeamID(UserIDToTeamID userIDToTeamID)
+        {
+            dbConnection.Insert(userIDToTeamID);
+            if (userIDsToTeamIDs != null)
+            {
+                userIDsToTeamIDs.Add(userIDToTeamID);
+            }
+        }
+
+        public bool DeleteUserIDToTeamID(UserIDToTeamID userIDToTeamID)
+        {
+            if (dbConnection.Delete(userIDToTeamID) == false)
+            {
+                return false;
+            }
+            else
+            {
+                if (userIDsToTeamIDs != null)
+                {
+                    userIDsToTeamIDs.Remove(userIDToTeamID);
+                }
+                return true;
+            }
+        }
+        protected abstract void CreateConnection();
+    }
+}
--- TeamManager.Service/Management/Database/ManagementSQLiteConnetion.cs
+++ TeamManager.Service/Management/Database/ManagementSQLiteConnetion.cs
@@ -6,148 +6,16 @@ using TeamManager.Service.Models;
 
 namespace TeamManager.Service.Management.Database
 {
-    public class ManagementSQLiteConnetion : IManagementDatabaseConnection
+    public class ManagementSQLiteConnetion : ManagementDatabaseManager
     {
-        string connString;
-        List<User> users = null;
-        List<Team> teams = null;
-        List<UserIDToTeamID> userIDsToTeamIDs = null;
-        public ManagementSQLiteConnetion(string connString)
+        public ManagementSQLiteConnetion(string connString) : base(connString)
         {
             this.connString = connString;
         }
 
-        public void SaveUser(User user)
+        protected override void CreateConnection()
         {
-            using (IDbConnection cnn = new SQLiteConnection(connString))
-            {
-                cnn.Insert(user);
-
-                if (users != null)
-                {
-                    users.Add(user);
-                }
-            }
-        }
-
-        public bool DeleteUser(User user)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(connString))
-            {
-                if (cnn.Delete(user) == false)
-                {
-                    return false;
-                }
-
-                else
-                {
-                    if (users != null)
-                    {
-                        users.Remove(user);
-                    }
-                    return true;
-                }
-            }
-        }
-
-        public List<User> GetAllUsers()
-        {
-            if (users == null)
-            {
-                using (IDbConnection cnn = new SQLiteConnection(connString))
-                {
-                    users = cnn.GetAll<User>().ToList();
-                }
-            }
-
-            return users;
-        }
-
-        public void SaveTeam(Team team)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(connString))
-            {
-                cnn.Insert(team);
-                if (teams != null)
-                {
-                    teams.Add(team);
-                }
-            }
-        }
-
-        public List<Team> GetAllTeams()
-        {
-            if (teams == null)
-            {
-                using (IDbConnection cnn = new SQLiteConnection(connString))
-                {
-                    teams = cnn.GetAll<Team>().ToList();
-                }
-            }
-
-            return teams;
-        }
-
-        public bool DeleteTeam(Team team)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(connString))
-            {
-                if (cnn.Delete(team) == false)
-                {
-                    return false;
-                }
-                else
-                {
-                    if (teams != null)
-                    {
-                        teams.Remove(team);
-                    }
-                    return true;
-                }
-            }
-        }
-
-        public List<UserIDToTeamID> GetAllUserIDToTeamID()
-        {
-            if (userIDsToTeamIDs == null)
-            {
-                using (IDbConnection cnn = new SQLiteConnection(connString))
-                {
-                    userIDsToTeamIDs = cnn.GetAll<UserIDToTeamID>().ToList();
-                }
-            }
-            return userIDsToTeamIDs;
-        }
-
-        public void SaveUserIDToTeamID(UserIDToTeamID userIDToTeamID)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(connString))
-            {
-                cnn.Insert(userIDToTeamID);
-                if (userIDsToTeamIDs != null)
-                {
-                    userIDsToTeamIDs.Add(userIDToTeamID);
-                }
-            }
-        }
-
-        public bool DeleteUserIDToTeamID(UserIDToTeamID userIDToTeamID)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(connString))
-            {
-                if (cnn.Delete(userIDToTeamID) == false)
-                {
-                    return false;
-                }
-                else
-                {
-                    if (userIDsToTeamIDs != null)
-                    {
-                        userIDsToTeamIDs.Remove(userIDToTeamID);
-                    }
-                    return true;
-                }
-            }
+            dbConnection = new SQLiteConnection(connString);
         }
     }
 }
